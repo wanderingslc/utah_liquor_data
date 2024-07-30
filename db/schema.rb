2a7712da65110bb5d33b22bae2c0983e62701a1e
Stan Martineau <Wanderingslc@gmail.com>
2024-07-30 15:14:36 -0600

    WIP

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,86 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 0) do
+ActiveRecord::Schema[7.2].define(version: 2024_07_29_194518) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "action_text_rich_texts", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "body"
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
+  end
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "businesses", force: :cascade do |t|
+    t.string "name"
+    t.string "phone"
+    t.string "address"
+    t.float "lat"
+    t.float "lng"
+    t.string "historical_license"
+    t.boolean "conditional"
+    t.date "orig_issue_date"
+    t.boolean "new_this_month"
+    t.boolean "active"
+    t.string "website"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "liquor_license_id", null: false
+    t.index ["liquor_license_id"], name: "index_businesses_on_liquor_license_id"
+  end
+
+  create_table "license_types", force: :cascade do |t|
+    t.string "name"
+    t.integer "alcohol_types"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "liquor_licenses", force: :cascade do |t|
+    t.string "license_number"
+    t.date "expiration_date"
+    t.string "dabc_link"
+    t.bigint "license_type_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["license_type_id"], name: "index_liquor_licenses_on_license_type_id"
+  end
+
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "businesses", "liquor_licenses"
+  add_foreign_key "liquor_licenses", "license_types"
 end
